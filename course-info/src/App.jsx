920352dfe0b9b864b8b7c959152e7dc89b097509
mansur <olaomansur@gmail.com>
2021-07-16 00:56:26 +0100

    EX1

--- course-info/src/App.jsx
+++ course-info/src/App.jsx
@@ -1,18 +1,35 @@
 import React, { useState } from "react";
 const App = () => {
-  const [left, setLeft] = useState(0)
-  const [right, setRight] = useState(0)
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const total = good + neutral + bad;
+  const allClicks = [good, neutral, bad];
+  const average = allClicks.reduce((acc, curr) => acc + curr, 0) / allClicks.length;
+  const positive = good === 0 ? 0 : (good * 100) / allClicks.reduce((acc, curr) => acc + curr, 0) + '%';
+
 
   return (
     <div>
-      {left}
-      <button onClick={() => setLeft(left + 1)}>
-        left
+      <h1>give feedback</h1>
+      <button onClick={() => setGood(good + 1)}>
+        Good
       </button>
-      <button onClick={() => setRight(right + 1)}>
-        right
+      <button onClick={() => setNeutral(neutral + 1)}>
+        neutral
       </button>
-      {right}
+      <button onClick={() => setBad(bad + 1)}>
+        Bad
+      </button>
+      <h1>Statistics</h1>
+      <div className="grid">
+        <span>Good</span>{good} <br />
+        <span>Neutral</span>{neutral} <br />
+        <span>Bad</span>{bad} <br />
+        <span>All Feedback</span> {total}<br />
+        <span>Average</span>{average} <br />
+        <span>Positive</span>{positive} <br />
+      </div>
     </div>
   );
 };
--- course-info/src/index.js
+++ course-info/src/index.js
@@ -1,5 +1,7 @@
 import ReactDom from "react-dom";
 
 import App from "./App";
+import ExTwo from "./ExTwo";
 
-ReactDom.render(<App />, document.getElementById("root"));
+ReactDom.render(<App />
+  , document.getElementById("root"));
